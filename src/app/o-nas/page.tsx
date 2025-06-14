5a8f78261d350921e5507be2da2f1edeba842242
Luke Sahula <lukas.sahula@icloud.com>
2025-06-15 00:16:15 +0200

    wip: other pages content

--- src/app/o-nas/page.tsx
+++ src/app/o-nas/page.tsx
@@ -1,22 +1,88 @@
 export default function AboutUs() {
   return (
-    <div className="text-center max-w-xl max-h-dvh md:gap-8 gap-4 py-4 flex flex-col">
-      <h1 className="text-3xl font-bold mb-6">
-        První český vyhledávač vzdělávání v&nbsp;první pomoci
-      </h1>
-      <p>Ještě to tu chystáme...</p>
+    <div className="text-center max-h-dvh md:gap-8 gap-4 py-4 flex flex-col">
+      <h1 className="text-3xl font-bold mb-6">O nás</h1>
       <p>
-        Kurzy první pomoci živě, online, knihy a další publikace, hry,
-        vzdělávací programy a weby – vše na jednom místě. Aby každý z nás věděl,
-        co dělat, když jde do tuhýho.
+        Jmenuju se Ellen, jsem vzděláním lékařka, která chtěla i na rodičáku
+        dělat něco, co má skutečný dopad. Jelikož sama učím první pomoc, často
+        vysvětluji, jak důležitá je prevence a vzdělávání se v první pomoci.
+        Přestože dokážu druhé nadchnout, uvědomila jsem si, že v mé snaze
+        popularizovat první pomoc chybí něco zásadního. Jedno místo, kam mohu
+        druhé odkázat, kde by každý snadno a rychle našel kurz či jiný typ
+        vzdělávání v první pomoci podle svých potřeb. A tak se zrodila myšlenka
+        na webový vyhledávač vzdělávání právě v první pomoci. A protože vím, že
+        Mám jen jeden, neotálela jsem a od stavění hradů z písku jsem začala
+        běhat k počítači. Teď se vyhledávač ještě peče, ale až bude na světě,
+        bude místem, kde si každý přijde na to svoje. Místem, kde
+        <br />
+        <br />
+        <strong>Karel</strong>, který jede s dětmi na tábor,{" "}
+        <strong>najde</strong> kurz s certifikátem zdravotníka zotavovacích
+        akcí,
+        <br />
+        <strong>Marie</strong>, ředitelka autoškoly, <strong>najde</strong>{" "}
+        školení první pomoci na míru pro své lektory jízd,
+        <br />
+        nastávající rodiče najdou kurz, který jim dá jistotu v péči o miminko,
+        <br />
+        <strong>Jana</strong>, která se chystá o prázdninách pracovat v Itálii,{" "}
+        <strong>najde</strong> kurz plavčíka,
+        <br />
+        <strong>Alois</strong>, ředitel základní školy, <strong>najde</strong>{" "}
+        hravý vzdělávací program pro žáky prvního i druhého stupně.
       </p>
-      <a
-        className="text-primary font-bold"
-        target="_blank"
-        href="mailto:zivot@masjenjeden.cz"
-      >
-        zivot@masjenjeden.cz
-      </a>
+      <p>
+        <strong>A kde najdete i vy!</strong>
+      </p>
+      <h2 className="text-2xl font-bold">
+        Spojuje nás touha <span className="underline">ten jeden, co máme</span>,
+        nepromarnit a zpřístupnit první pomoc úplně každému. <br />
+        Jednoduše, s pár kliky, díky kterým zachráníte život.
+      </h2>
+      <div className="flex text-left px-8 gap-8 justify-between">
+        <div className="flex-2/5">
+          <h3 className="text-xl mb-2">
+            Jsme <strong>Umím První Pomoc</strong>
+          </h3>
+          <p>první český vyhledávač vzdělávání v první pomoci</p>
+          <br />
+          <p>
+            Tvoříme jedno místo, kde si každý z nás najde vzdělávání v první
+            pomoci podle svých potřeb. Kurzy, online, knihy a další publikace,
+            hry, vzdělávací programy a weby. Živě, online, veřejně, soukromě.
+            Pro mámy, táty, tety, strejdy, dědy, babičky, děti, jednotlivce i
+            týmy. Aby každý z nás věděl, co dělat, když jde do tuhýho.
+          </p>
+          <br />
+          <p>
+            Děláme to pro sebe, pro svourodinu a blízké i pro ty, které potkáme
+            na ulici. Spolu umíme první pomoc. Spolu zachraňujeme životy.
+          </p>
+        </div>
+        <div className="flex-2/5">
+          <ul className="list-disc">
+            <li>
+              <strong>Popularizujeme</strong> - šíříme povědomí o první pomoci
+              napříč společností od dětí po seniory, mezi jednotlivci i týmy,
+              mezi laiky i odborníky
+            </li>
+            <li>
+              <strong>Zpřístupňujeme</strong> - zlepšujeme dostupnost vzdělávání
+              v první pomoci, propojujeme zájemce o kurzy a další s ověřenými
+              poskytovateli
+            </li>
+            <li>
+              <strong>Detabuizujeme a demýtizujeme</strong> - mluvíme o první
+              pomoci nahlas, bez studu, protože i laická záchrana života má
+              obrovský smysl
+            </li>
+          </ul>
+        </div>
+      </div>
+      <h2 className="text-2xl font-bold">Náš tým</h2>
+      <p>
+        Ve dvou se to lépe táhne. A stále hledáme další parťáky. Napište nám.
+      </p>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,17 @@
 export default function Home() {
   return (
-    <div className="text-center max-w-xl max-h-dvh md:gap-8 gap-4 py-4 flex flex-col">
-      <h1 className="text-3xl font-bold mb-6">
+    <div className="text-center max-h-dvh md:gap-8 gap-4 py-4 flex flex-col">
+      <h1 className="text-5xl font-bold mb-6">
         První český vyhledávač vzdělávání v&nbsp;první pomoci
       </h1>
-      <p>Ještě to tu chystáme...</p>
+      <button className="bg-accent text-white text-3xl rounded-4xl max-w-md self-center px-8 py-4 font-bold">
+        Chci umět pomoci
+      </button>
       <p>
         Kurzy první pomoci živě, online, knihy a další publikace, hry,
         vzdělávací programy a weby – vše na jednom místě. Aby každý z nás věděl,
         co dělat, když jde do tuhýho.
       </p>
-      <a
-        className="text-primary font-bold"
-        target="_blank"
-        href="mailto:zivot@masjenjeden.cz"
-      >
-        zivot@masjenjeden.cz
-      </a>
     </div>
   );
 }
