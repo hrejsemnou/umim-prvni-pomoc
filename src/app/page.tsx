29a6d81488102a48286085f75dbb7ac55452cc56
Luke Sahula <lukas.sahula@icloud.com>
2025-06-19 22:59:42 +0200

    Fix homepage font sizes overflowing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,25 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <div className="px-12 h-screen md:gap-8 gap-4 py-10 flex flex-col justify-center items-center w-full z-[1]">
-        <p className="text-background font-bold">
+      <div className="px-12 text-center h-dvh gap-3 py-10 flex flex-col justify-center items-center w-full z-[1]">
+        <p className="text-background md:text-lg text-sm">
           Aby každý věděl, co dělat, když jde do tuhýho
         </p>
-        <h1 className="text-5xl font-bold text-background text-center md:max-w-6/12">
+        <h1 className="md:text-4xl text-2xl font-bold text-background md:max-w-6/12">
           Tvoříme první český vyhledávač vzdělávání v&nbsp;první pomoci
         </h1>
         <Link
           href="/vyhledavac"
-          className="bg-primary text-background font-bold text-lg text-nowrap px-8 py-4 rounded-4xl"
+          className="mt-4 mb-4 bg-primary text-background font-bold text-lg text-nowrap px-8 py-4 rounded-4xl"
         >
           Umět zachránit život
         </Link>
-        <p className="text-lg text-background">
+        <p className="md:text-lg text-sm text-background">
           Kurzy první pomoci živě, online, knihy a další publikace, hry,
-          vzdělávací programy a události - vše na jednom místě.
+          vzdělávací programy a události - <strong>vše na jednom místě</strong>.
         </p>
       </div>
-      <div className="h-screen w-full overflow-hidden absolute z-0">
+      <div className="h-dvh w-full overflow-hidden absolute z-0">
         <Image
           src="/images/background.png"
           fill
