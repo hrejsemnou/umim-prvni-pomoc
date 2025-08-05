1985c4c2b76fda66e512cd23be55474a173bf7fb
Lukáš Hrejsemnou <lukas.hrejsemnou@gmail.com>
2025-08-05 10:25:22 +0200

    Disable submit button when submitting

--- src/features/educationProviders/components/StepNavigation.tsx
+++ src/features/educationProviders/components/StepNavigation.tsx
@@ -1,3 +1,5 @@
+import { useFormContext } from "react-hook-form";
+
 interface StepNavigationProps {
   step: number;
   totalSteps: number;
@@ -10,32 +12,37 @@ export const StepNavigation = ({
   totalSteps,
   onBack,
   onNext,
-}: StepNavigationProps) => (
-  <div className={`flex ${step === 0 ? "justify-end" : "justify-between"}`}>
-    {step > 0 && (
-      <button
-        type="button"
-        onClick={onBack}
-        className="text-background font-bold color-foreground bg-accent rounded-[8px] py-2 px-4 cursor-pointer"
-      >
-        Zpět
-      </button>
-    )}
-    {step < totalSteps - 1 ? (
-      <button
-        type="button"
-        onClick={onNext}
-        className="text-background font-bold color-foreground bg-primary rounded-[8px] py-2 px-4 cursor-pointer"
-      >
-        Další
-      </button>
-    ) : (
-      <button
-        type="submit"
-        className="text-background font-bold color-foreground bg-primary rounded-[8px] py-2 px-4 cursor-pointer"
-      >
-        Odeslat
-      </button>
-    )}
-  </div>
-);
+}: StepNavigationProps) => {
+  const { formState } = useFormContext();
+
+  return (
+    <div className={`flex ${step === 0 ? "justify-end" : "justify-between"}`}>
+      {step > 0 && (
+        <button
+          type="button"
+          onClick={onBack}
+          className="text-background font-bold color-foreground bg-accent rounded-[8px] py-2 px-4 cursor-pointer"
+        >
+          Zpět
+        </button>
+      )}
+      {step < totalSteps - 1 ? (
+        <button
+          type="button"
+          onClick={onNext}
+          className="text-background font-bold color-foreground bg-primary rounded-[8px] py-2 px-4 cursor-pointer"
+        >
+          Další
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className="text-background font-bold color-foreground bg-primary rounded-[8px] py-2 px-4 cursor-pointer disabled:opacity-50"
+          disabled={formState.isSubmitting}
+        >
+          Odeslat
+        </button>
+      )}
+    </div>
+  );
+};
